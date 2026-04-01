6a0a9095e4d650d01ca2df480cad3e5d9dcbb21d
KNTy1018 <soramametaro1018@gmail.com>
2026-04-01 18:58:15 +0900

    [delete]スクリプト等削除

--- Assets/Scripts/Character/CharacterDefinition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CharacterDefinition", menuName = "Definition/CharacterDefinition")]
-public class CharacterDefinition : ScriptableObject
-{
-    [SerializeField] CharacterType _characterType;
-
-    public CharacterType characterType => _characterType;
-}
