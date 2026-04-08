ee8f589f2671eb1d5a8093581af2dfd41e8fa045
KNTy1018 <soramametaro1018@gmail.com>
2026-04-09 00:33:53 +0900

    [fix]インタラクト用のクラスに修正

--- Assets/Scripts/Interact/View/InteractView_Input.cs
+++ Assets/Scripts/Interact/View/InteractView_Input.cs
@@ -1,36 +1,7 @@
 using MVPTools.Runtime;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public partial class InteractView
 {
-    void Enter(InputAction.CallbackContext ctx)
-    {
-
-    }
-
-    void Cancel(InputAction.CallbackContext ctx)
-    {
-
-    }
-
-    void SelectItem(InputAction.CallbackContext ctx)
-    {
-
-    }
-
-    void NextItem(InputAction.CallbackContext ctx)
-    {
-
-    }
-
-    void BackItem(InputAction.CallbackContext ctx)
-    {
-
-    }
-
-    void Menu(InputAction.CallbackContext ctx)
-    {
-
-    }
+    //必要な場合にViewの入力部分を実装
 }
