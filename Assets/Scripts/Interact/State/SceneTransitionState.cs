028808a86cf448e87528357a1295b8849a0fc51b
KNTy1018 <soramametaro1018@gmail.com>
2026-04-29 15:04:38 +0900

    [add]シーン遷移イベント

---
+++ Assets/Scripts/Interact/State/SceneTransitionState.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SceneTransitionState : InteractStateBase
+{
+    public SceneTransitionState(InteractView view, InteractPresenter presenter, InteractRuntime runtime) : base(view, presenter, runtime)
+    {
+    }
+
+    public override void Entry()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void MoveIndex(SlotMove move)
+    {
+
+    }
+
+    public override void PushEnter()
+    {
+
+    }
+
+    public override void SelectIndex(int index)
+    {
+
+    }
+}
